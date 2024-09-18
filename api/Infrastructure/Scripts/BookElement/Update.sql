248ab661b1bc859875f035740b8374ee544bb61b
valvixx <valvius04@gmail.com>
2024-09-19 00:43:57 +0300

    scripts and bookelement upd

--- api/Infrastructure/Scripts/BookElement/Update.sql
+++ api/Infrastructure/Scripts/BookElement/Update.sql
@@ -1,23 +1,23 @@
 ﻿UPDATE book_elements be
 SET
-    be.type        = @Type,
-    be.name        = @Name,
-    be.description = @Description
-    be.reason      = @Reason
-    be.population  = @Population
-    be.family      = @Family
-    be.appearance  = @Appearance
-    be.behavior    = @Behavior
-    be.nutrition   = @Nutrition
+    type        = @Type,
+    name        = @Name,
+    description = @Description,
+    reason      = @Reason,
+    population  = @Population,
+    family      = @Family,
+    appearance  = @Appearance,
+    behavior    = @Behavior,
+    nutrition   = @Nutrition
 WHERE id = @Id
 RETURNING
     be.id          as "Id",
     be.type        as "Type",
     be.name        as "Name",
-    be.description as "Description"
-    be.reason      as "Reason"
-    be.population  as "Population"
-    be.family      as "Family"
-    be.appearance  as "Appearance"
-    be.behavior    as "Behavior"
+    be.description as "Description",
+    be.reason      as "Reason",
+    be.population  as "Population",
+    be.family      as "Family",
+    be.appearance  as "Appearance",
+    be.behavior    as "Behavior",
     be.nutrition   as "Nutrition"
